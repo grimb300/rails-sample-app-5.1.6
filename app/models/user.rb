e06c5919f4bef1f215db35cafd8a51b094af96c2
Bob Grim <grimb300@hotmail.com>
2019-07-01 16:00:43 +0200

    data validation on user.name and user.email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A(\w+[+\-.]?)+@([a-z\d\-]+\.?)+[a-z]+\z/i
+  # I like MY_REGEX better since it seems cleaner
+  #MY_REGEX          = /\A(\w+[+\-.]?)+@([a-z\d\-]+\.?)+[a-z]+\z/i
+  #TUTORIAL_REGEX    = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true,
+                    length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: true
 end
