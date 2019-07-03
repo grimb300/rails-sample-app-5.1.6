fb367604c91271b8cf3c16ba0fac2d18a6dd759c
Bob Grim <grimb300@hotmail.com>
2019-07-03 11:06:12 +0200

    handle invalid login

---
+++ test/integration/users_login_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class UsersLoginTest < ActionDispatch::IntegrationTest
+  test "login with invalid information" do
+    # 1. Visit the login path.
+    get login_path
+    # 2. Verify that the new sessions form renders properly.
+    assert_template 'sessions/new'
+    # 3. Post to the sessions path with an invalid params hash.
+    post login_path, params: { session: { email: "", password: "" } }
+    # 4. Verify that the new sessions form gets re-rendered and that a flash message appears.
+    assert_template 'sessions/new'
+    assert_not flash.empty?
+    # 5. Visit another page (such as the Home page).
+    get root_path
+    # 6. Verify that the flash message doesn’t appear on the new page.
+    assert flash.empty?
+  end
+end
